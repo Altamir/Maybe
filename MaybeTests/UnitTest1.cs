2d8baf945edaae267d54d4b697431913f975ee2b
Altamir Benkenstein <altamir.benkenstein@outlook.com>
2018-03-01 22:26:23 -0300

    fixed test

--- MaybeTests/UnitTest1.cs
+++ MaybeTests/UnitTest1.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Alt.Utils.Generics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -63,7 +62,5 @@ namespace MaybeTests
             Assert.IsTrue(mayCar.HasValue);
             Assert.AreEqual(mayCar.Value.name, "car test");
         }
-
-
     }
 }
